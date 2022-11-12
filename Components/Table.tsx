986ddaad7ec4aaa8304cf699f2b1e83bd60b8957
shinhyunsu01 <makelabel@naver.com>
2022-11-12 13:45:59 +0900

    bold

--- Components/Table.tsx
+++ Components/Table.tsx
@@ -22,19 +22,19 @@ export const Table = ({ data, onClick }: Ipros) => {
 						onClick={() => onClick(index)}
 						className=" h-12 hover:bg-slate-100 hover:cursor-pointer border-b-2"
 					>
-						<td>{ele.tag}</td>
-						<td>{ele.name}</td>
+						<td className="text-gray-600">{ele.tag}</td>
+						<td className="font-bold">{ele.name}</td>
 						<td>
 							<div className="flex items-center justify-center">
 								{ele.amount <= ele.minamount ? (
 									<div className="h-4 w-4 rounded-full bg-orange-500"></div>
 								) : null}
 
-								<span>
+								<span className="font-bold">
 									&nbsp;
 									{ele.amount}
-									{ele.unit}
 								</span>
+								<span>{ele.unit}</span>
 							</div>
 						</td>
 					</tr>
